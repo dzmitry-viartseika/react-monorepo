12def0a6816d6e14bcd7d0045351ae76710a1095
Dzmitry Viartseika <verteyko1990@gmail.com>
2024-04-21 22:53:42 +0200

    add libraries

---
+++ apps/task-management/src/app/app.tsx
@@ -0,0 +1,13 @@
+
+import {TaskList} from '@react-monorepo/task-list';
+
+export function App() {
+  return (
+    <div>
+      <h1>Task Management</h1>
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
